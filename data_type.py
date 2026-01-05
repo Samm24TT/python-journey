c1d7826551db459eda213c6597099ad8c363f259
Sam24 <aclian4321@gmail.com>
2026-01-05 23:26:52 +0700

    finish learning indexing and slicing in python

---
+++ data_type.py
@@ -0,0 +1,20 @@
+# name = "pete"
+# print(name)
+
+# type_of = type(name)
+# print(type_of)
+
+isover18 = True
+print(type(isover18))
+
+name = 'samuel bawi nei lian'
+print(name[0:6])
+
+copyName = name[::-1]
+print(copyName)
+
+# Data type
+
+# number ==> 1. Integer 2. float
+# string ==> text
+# boolean ==> true and false
