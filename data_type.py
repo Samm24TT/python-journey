9976c00a705211e5c78c7f4c769749fa6095eca3
Sam24 <aclian4321@gmail.com>
2026-01-06 22:44:33 +0700

    doing datatypes

--- data_type.py
+++ data_type.py
@@ -1,20 +1,68 @@
-# name = "pete"
-# print(name)
+# literal assignment
+first = "john"
+last = "smith"
 
-# type_of = type(name)
-# print(type_of)
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
 
-isover18 = True
-print(type(isover18))
+# constructure function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
 
-name = 'samuel bawi nei lian'
-print(name[0:6])
+# Concatination
+fullname = first + " " + last
+print(fullname)
 
-copyName = name[::-1]
-print(copyName)
+fullname += "!"
+print(fullname)
 
-# Data type
+# Casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
 
-# number ==> 1. Integer 2. float
-# string ==> text
-# boolean ==> true and false
+statement = "I like country song form the " + decade + "s."
+print(statement)
+
+# Multiple lines
+multiline = '''
+hello, Mr.Python
+
+nice to meet you
+
+               good nice!
+'''
+print(multiline)
+
+# Escaping special characters
+sentence = 'I\'m back at work!\tHey\n\nWhere\'s this at\\located?'
+print(sentence)
+
+# String methods
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
+
+print(multiline.title())
+print(multiline.replace("good", "ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                           "
+multiline = "                          " + multiline
+print(len(multiline))
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+print('')
+
+# Build a menu
+title = "menu".upper()
+print(title.center(20, "="))
+print("Coffee".ljust(16, ".") + "$1".rjust(4))
