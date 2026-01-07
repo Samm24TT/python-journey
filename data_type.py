ce869ca8c12ba9423fe6c24cdb4667f7b0b6961d
Sam24 <aclian4321@gmail.com>
2026-01-07 15:52:33 +0700

    finish learning datatype of pyhton

--- data_type.py
+++ data_type.py
@@ -1,4 +1,5 @@
 # literal assignment
+import math
 first = "john"
 last = "smith"
 
@@ -66,3 +67,61 @@ print('')
 title = "menu".upper()
 print(title.center(20, "="))
 print("Coffee".ljust(16, ".") + "$1".rjust(4))
+print("Muffin".ljust(16, ".") + "$2".rjust(4))
+print("Cheesecake".ljust(16, ".") + "$4".rjust(4))
+
+print('')
+
+# string inex value
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# Some method return boolean data
+print(first.startswith("D"))
+print(first.endswith("Z"))
+
+# Boolean data type
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data type
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+# float type
+gpa = 3.20
+y = float(1.14)
+print(type(gpa))
+
+print('')
+
+# complex type
+comp_value = 5 + 2j
+print(comp_value)
+print(comp_value.real)
+print(comp_value.imag)
+
+# Built-in functions for numbers
+print(abs(gpa))
+print(round(gpa))
+print(round(gpa, 1))
+
+# math
+print(math.pi)
+print(math.sqrt(144))
+print(math.floor(gpa))
+print(math.ceil(gpa))
+
+# Casting a string to a number
+zipcode = '10010'
+zip_value = int(zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data
+# zip_value = int("New York")
